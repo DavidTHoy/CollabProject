b2dd3619e1ad552b07b0df3e3f5cb94a666c7f36
xksmitty20 <xavierparkersmith@yahoo.com>
2018-01-02 13:57:47 -0500

    Add files via upload

---
+++ Collaboration_Project/collabproject/collabproject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for collabproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collabproject.settings")
+
+application = get_wsgi_application()
