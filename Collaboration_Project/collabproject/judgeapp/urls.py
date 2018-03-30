f3ef8c07808ccb3db07d3ccde736196b118309ce
DavidTHoy <33630499+DavidTHoy@users.noreply.github.com>
2018-03-30 15:35:40 -0400

    Team/Judge layout Fixes

--- Collaboration_Project/collabproject/judgeapp/urls.py
+++ Collaboration_Project/collabproject/judgeapp/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$',views.index, name='index'),
-    path('/judge-score/', views.score, name='score'),
-    path('/judge-timeslot/', views.timeslot, name='timeslot'),
-    path('/judge-timeslot/results', views.results, name='results'),
-    path('/register', views.register_view, name='register'),
-    path('/login', views.login_view, name='login'),
-    path('/logout', views.logout_view, name='logout'),
+    url(r'^$',views.index, name='judge-home'),
+    path('judge-score/', views.score, name='judge-score'),
+    path('judge-timeslot/', views.timeslot, name='judge-timeslot'),
+    path('judge-timeslot/results', views.results, name='judge-results'),
+    path('register', views.register_view, name='judge-register'),
+    path('login', views.login_view, name='judge-login'),
+    path('logout', views.logout_view, name='judge-logout'),
 
 
 ]
--- Collaboration_Project/collabproject/teamapp/urls.py
+++ Collaboration_Project/collabproject/teamapp/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    path('/team-assignment/', views.assignment, name='assignment'),
-    path('/team-member/', views.members, name='members'),
-    path('/team-score/', views.score, name='score'),
-    path('/team-topic/', views.topic, name='topic'),
+    url(r'^$', views.index, name='team-menu'),
+    path('team-assignment/', views.assignment, name='team-assignment'),
+    path('team-member/', views.members, name='team-members'),
+    path('team-score/', views.score, name='team-score'),
+    path('team-topic/', views.topic, name='team-topic'),
 
 ]
